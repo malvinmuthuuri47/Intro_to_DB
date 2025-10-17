055cfcb27509be9204997cd159771159a94dfc57
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-10-17 15:33:14 +0300

    Added solution to the populating database task

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- This script populates a db table with one row
+
+USE DATABASE();
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, "cole Baidoo", "cbaidoo@sandtech.com", "123 appiness Ave.");
