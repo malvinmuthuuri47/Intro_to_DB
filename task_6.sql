bba908c3af4988eb380653626378dbedc2aed70c
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-10-17 15:39:20 +0300

    Added solution to the task requiring populating db with multiple rows

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+--- This script inserts multiple rows of data in a database table
+
+USE DATABASE();
+
+INSERT INTO customer VALUES
+(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+(3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.");
