6f558805caf9a5e494561d89484aa1c8b9e8a233
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-10-17 15:34:10 +0300

    Updated task_5.sql to have the correct address

--- task_5.sql
+++ task_5.sql
@@ -3,4 +3,4 @@
 USE DATABASE();
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, "cole Baidoo", "cbaidoo@sandtech.com", "123 appiness Ave.");
+VALUES (1, "cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
